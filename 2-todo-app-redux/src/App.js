35a6b2cb81b5ac68bc829519a587f6d889ebe409
Yunus YILMAZ <yunusyilmazz34@gmail.com>
2023-03-28 18:01:29 +0300

    added components

--- 2-todo-app-redux/src/App.js
+++ 2-todo-app-redux/src/App.js
@@ -1,9 +1,15 @@
-import "./App.css";
+import { TodoList } from "./components/TodoList";
+import { Form } from "./components/Form";
+import "./styles/index.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">React Todo App With Redux</header>
+      <header className="App-header text-center">
+        React Todo App With Redux
+      </header>
+      <Form />
+      <TodoList />
     </div>
   );
 }

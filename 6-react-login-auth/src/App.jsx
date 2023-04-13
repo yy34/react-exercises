87e016e5ed100c0a0456a85347cdcb292ee5876e
Yunus YILMAZ <yunusyilmazz34@gmail.com>
2023-04-14 00:02:09 +0300

    added router

--- 6-react-login-auth/src/App.jsx
+++ 6-react-login-auth/src/App.jsx
@@ -1,10 +1,28 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Home } from "./pages/Home/index";
+import { Header } from "./components/Header";
+import { Login } from "./components/Login";
+import { Register } from "./components/Register";
 import "./App.css";
-
 function App() {
   return (
-    <div className="App">
-      <h1>React Login Authentication with JWT</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 

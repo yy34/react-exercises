51e22b6f8e1e55280dc6863aff02a6f9988a5af1
Yunus YILMAZ <yunusyilmazz34@gmail.com>
2023-04-15 15:08:37 +0300

    added chakra & context

--- 6-react-login-auth/src/App.jsx
+++ 6-react-login-auth/src/App.jsx
@@ -1,28 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Home } from "./pages/Home/index";
 import { Header } from "./components/Header";
 import { Login } from "./components/Login";
 import { Register } from "./components/Register";
+import { MainContext } from "./context/Context";
+import { ChakraProvider } from "@chakra-ui/react";
+
 import "./App.css";
 function App() {
+  const [login, setlogin] = useState(false);
+  const data = {
+    login,
+    setlogin,
+  };
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route path="/">
-            <Header />
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <ChakraProvider>
+      <MainContext.Provider value={data}>
+        <BrowserRouter>
+          <div className="App">
+            <Switch>
+              <Route path="/">
+                <Header />
+                <Home />
+              </Route>
+              <Route path="/login">
+                <Login />
+              </Route>
+              <Route path="/register">
+                <Register />
+              </Route>
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </MainContext.Provider>
+    </ChakraProvider>
   );
 }
 
--- 6-react-login-auth/src/components/Header.jsx
+++ 6-react-login-auth/src/components/Header.jsx
@@ -1,5 +1,16 @@
 import React from "react";
-
+import { MainContext, useContext } from "../context/Context";
+import { Button, ButtonGroup, Stack } from "@chakra-ui/react";
 export const Header = () => {
-  return <div>Header</div>;
+  const data = useContext(MainContext);
+  return (
+    <div className="Appn">
+      <Stack spacing={4} direction="row" align="center">
+        <ButtonGroup variant="outline" spacing="6">
+          <Button colorScheme="blue">Login</Button>
+          <Button>Register</Button>
+        </ButtonGroup>
+      </Stack>
+    </div>
+  );
 };
